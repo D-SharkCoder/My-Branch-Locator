6c7ac4ec52130870a216457422cffc2166780531
Mark Anthony Santos <markanthonysantos2497@gmail.com>
2025-08-07 01:10:31 +0800

    update

--- backend/app/Http/Controllers/BranchController.php
+++ backend/app/Http/Controllers/BranchController.php
@@ -8,7 +8,6 @@ use App\Http\Requests\UpdateBranchRequest;
 use App\Models\Branch;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Cache;
 
 class BranchController extends Controller
 {
@@ -18,27 +17,24 @@ class BranchController extends Controller
         $for = $request->input('for') ?? null;
         $sort = $request->input('sort') ?? null;
         
-        return Cache::remember("branches-{$request->ip()}", 3600, function () use ($text, $for, $sort) {
-            return Branch::when($text && $for == 'any', function ($query) use ($text) {
-                $query->where("name", 'LIKE', "%${text}%")
-                ->orWhere("manager", "LIKE", "%${text}%")
-                ->orWhere("phone", "LIKE", "%${text}%")
-                ->orWhere("address", "LIKE", "%${text}%");
-            })
-            ->when($text && $for != 'any', function ($query) use ($for, $text) {
-                $query->where($for, 'LIKE', "%${text}%");
-            })
-            ->when(in_array($sort, ['asc', 'desc']), function ($query) use ($sort) {
-                $query->orderBy('name', $sort);
-            })
-            ->get();
-        });
+        return Branch::when($text && $for == 'any', function ($query) use ($text) {
+            $query->where("name", 'LIKE', "%${text}%")
+            ->orWhere("manager", "LIKE", "%${text}%")
+            ->orWhere("phone", "LIKE", "%${text}%")
+            ->orWhere("address", "LIKE", "%${text}%");
+        })
+        ->when($text && $for != 'any', function ($query) use ($for, $text) {
+            $query->where($for, 'LIKE', "%${text}%");
+        })
+        ->when(in_array($sort, ['asc', 'desc']), function ($query) use ($sort) {
+            $query->orderBy('name', $sort);
+        })
+        ->get();
     }
 
     public function store(NewBranchRequest $request)
     {
         $branch = Branch::create($request->validated());
-        Cache::forget("branches-{$request->ip()}");
 
         return response()->json($branch, Response::HTTP_CREATED);
     }
@@ -51,15 +47,13 @@ class BranchController extends Controller
     public function update(UpdateBranchRequest $request, Branch $branch)
     {
         $branch->update($request->validated());
-        Cache::forget("branches-{$request->ip()}");
 
         return response()->json($branch);
     }
 
-    public function destroy(Branch $branch, Request $request)
+    public function destroy(Branch $branch)
     {
         $branch->delete();
-        Cache::forget("branches-{$request->ip()}");
 
         return response()->json(null, Response::HTTP_NO_CONTENT);
     }
